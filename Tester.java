5cdbf5d68570176905bd2cc56ee257cc5ac5aae2
vianalin <vlin21@stuy.edu>
2020-10-28 18:32:44 -0400

    tested getbal and getid

--- Tester.java
+++ Tester.java
@@ -1,3 +1,7 @@
 public class Tester{
-	
+	public static void main(String[] args) {
+		BankAccount a = new BankAccount(100, "hi world");
+		System.out.println(a.getBalance());
+		System.out.println(a.getAccountID());
+	}
 }
