950565cb82576f02c0499428ee31b775610f3bcb
vianalin <vlin21@stuy.edu>
2020-10-28 18:37:08 -0400

    tested get and setpass

--- Tester.java
+++ Tester.java
@@ -1,7 +1,14 @@
 public class Tester{
 	public static void main(String[] args) {
 		BankAccount a = new BankAccount(100, "hi world");
-		System.out.println(a.getBalance());
-		System.out.println(a.getAccountID());
+
+		System.out.println(a.getBalance()); //bal
+
+		System.out.println(a.getAccountID()); //id
+
+		System.out.println(a.getPassword()); //testing getpass
+		a.setPassword("hello world"); //changing pass
+		System.out.println(a.getPassword()); //testing setpass
+
 	}
 }
